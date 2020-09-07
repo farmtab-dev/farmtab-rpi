1f43b1b494230c1bdbb8ad19d7523e0408474b30
farmtab-dev <farmtab.app@gmail.com>
2020-09-07 16:37:13 +0800

    Change duration of camera interval

--- camera/config/cfg_js_time.js
+++ camera/config/cfg_js_time.js
@@ -29,8 +29,8 @@ config.sleep = (milliseconds) => {
 }
 
 config.CRON_JOB_INTERVAL = process.env.INTERVAL_CRON || TIME_VALUE.hour * 3;
-config.CHANGE_CAM_INTERVAL = process.env.INTERVAL_CHG_CAM || TIME_VALUE.sec * 10;
-config.UPDATE_CAM_FEED = process.env.INTERVAL_CAP_IMG || TIME_VALUE.sec * 5;
+config.CHANGE_CAM_INTERVAL = process.env.INTERVAL_CHG_CAM || TIME_VALUE.sec * 30;
+config.UPDATE_CAM_FEED = process.env.INTERVAL_CAP_IMG || TIME_VALUE.sec * 10;
 // config.SEND_IMG_HOUR = [6, 9, 12, 15];
 config.SEND_IMG_HOUR = [7, 10, 13, 16];
 
