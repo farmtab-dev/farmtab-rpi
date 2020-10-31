798b398ce20a8e00e9ac32b10ca649fcd18e09c8
farmtab-dev <farmtab.app@gmail.com>
2020-11-01 01:08:20 +0800

    Arducam

--- camera/picam_python/func/h_arducam_func.py
+++ camera/picam_python/func/h_arducam_func.py
@@ -1,10 +1,13 @@
 import RPi.GPIO as gp
 import os
 from time import sleep
-def changeCam(cam, isInit= False):
+
+
+def changeCam(cam, isInit=False):
+    gp.setwarnings(True)
     if (not isInit):
         gp.cleanup()
-    gp.setwarnings(True)
+        print("Exec cleanup")
     gp.setmode(gp.BOARD)
 
     gp.setup(7, gp.OUT)
@@ -21,47 +24,49 @@ def changeCam(cam, isInit= False):
     gp.output(16, True)
     gp.output(21, True)
     gp.output(22, True)
-    if (cam=="A"):
+    if (cam == "A"):
         print("Change to camera A")
         i2c = "i2cset -y 1 0x70 0x00 0x04"
         os.system(i2c)
         gp.output(7, False)
         gp.output(11, False)
         gp.output(12, True)
-    elif(cam=="B"):
-        print("Change to camera B"   )
+    elif(cam == "B"):
+        print("Change to camera B")
         i2c = "i2cset -y 1 0x70 0x00 0x05"
         os.system(i2c)
         gp.output(7, True)
         gp.output(11, False)
         gp.output(12, True)
-    elif(cam=="C"):
+    elif(cam == "C"):
         print("Change to camera C")
         i2c = "i2cset -y 1 0x70 0x00 0x06"
         os.system(i2c)
         gp.output(7, False)
         gp.output(11, True)
         gp.output(12, False)
-    elif(cam=="D"):
+    elif(cam == "D"):
         print("Change to camera D")
         i2c = "i2cset -y 1 0x70 0x00 0x07"
         os.system(i2c)
         gp.output(7, True)
         gp.output(11, True)
         gp.output(12, False)
-    
+
 
 # https://www.raspberrypi.org/forums/viewtopic.php?t=56086
-#https://www.raspberrypi.org/forums/viewtopic.php?t=67175#:~:text=raspistill%20will%20exit%20after%205s,0%22%20to%20disable%20this%20timeout.
+# https://www.raspberrypi.org/forums/viewtopic.php?t=67175#:~:text=raspistill%20will%20exit%20after%205s,0%22%20to%20disable%20this%20timeout.
 def captureImg(filepath, width, height, rotate, cam, cap_timeout):
     if (rotate):
         # cmd = "raspistill -t 0 -s -o "+ filepath + " -w " +str(width) +" -h "+ str(height)+" -rot 180"
-        cmd = "raspistill -t "+str(cap_timeout)+" -o "+ filepath + " -w " +str(width) +" -h "+ str(height)+" -rot 180"
+        cmd = "raspistill -t "+str(cap_timeout)+" -o " + filepath + \
+            " -w " + str(width) + " -h " + str(height)+" -rot 180"
     else:
         # cmd = "raspistill  -t 0 -s -o "+ filepath + " -w " +str(width) +" -h "+ str(height)
-        cmd = "raspistill  -t "+str(cap_timeout)+" -o "+ filepath + " -w " +str(width) +" -h "+ str(height)
+        cmd = "raspistill  -t " + \
+            str(cap_timeout)+" -o " + filepath + \
+            " -w " + str(width) + " -h " + str(height)
 
     os.system(cmd)
     # sleep(3)
     # gp.cleanup()
-
